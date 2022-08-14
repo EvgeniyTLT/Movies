1405acb54b14bbfd406bdffb01fd9993ef9f0e14
EvgeniyTLT <Toy444@ya.ru>
2022-08-14 10:45:46 +0300

    add

--- app/src/main/java/com/example/movies/MainActivity.kt
+++ app/src/main/java/com/example/movies/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.movies
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnClickMe= findViewById<Button>(R.id.main_activity_register_button)
+
+        btnClickMe.setOnClickListener {
+            val registerButton = Intent(this, RegistrationActivity::class.java)
+            startActivity(registerButton)
+            Toast.makeText(this, "you clicked me.", Toast.LENGTH_SHORT).show()
+        }
+
+        val btnClickMe2 = findViewById<TextView>(R.id.main_activity_login_text)
+        btnClickMe2.setOnClickListener {
+            val loginButton = Intent(this, LoginActivity::class.java)
+            startActivity(loginButton)
+        }
     }
 }
