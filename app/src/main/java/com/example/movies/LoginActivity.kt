1f1d9b117f9fcb0dfef8bc10acf7edaa65608ab3
EvgeniyTLT <Toy444@ya.ru>
2022-08-15 15:19:36 +0300

    add

--- app/src/main/java/com/example/movies/LoginActivity.kt
+++ app/src/main/java/com/example/movies/LoginActivity.kt
@@ -2,10 +2,16 @@ package com.example.movies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+//        val button_exit = findViewById<ImageButton>(R.id.imageExit)
+//        button_exit.setOnClickListener {
+//            finish()
+//        }
     }
 }
